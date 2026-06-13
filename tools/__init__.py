39fab4926218030da955c391e632df902a6fe33b
rukaaah <pedrolucas.cremonini@gmail.com>
2026-06-13 10:28:47 -0400

    feat: Criação de testes, correção de ia e adição do ultimo tópico

--- tools/__init__.py
+++
@@ -1,8 +0,0 @@
-from .agenda import (
-    consultar_agenda, listar_tarefas, adicionar_tarefa, concluir_tarefa,
-    adicionar_evento_agenda, remover_evento_agenda
-)
-from .rag_tool import buscar_material_rag
-from .executor import execute_tool_call
-from .description import tools_description
-from .learning import gerar_exercicios, recomendar_revisao
